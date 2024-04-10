c4c172d17ac98fe95646314128f9afdcf11ca855
TanawatJukmongkol <tanawat.jukmon@gmail.com>
2024-04-10 22:15:06 +0700

    done

--- supun-final.ino
+++ supun-final.ino
@@ -4,32 +4,22 @@
 WiFiConnectionHandler ArduinoIoTPreferredConnection(WIFI_SSID, WIFI_PASS);
 
 // Variables
-bool alarm_on = true;
-
-// Sensor globals
-float sensor = 0;
-bool reset_btn = false;
-
-void onResetBtnChange()
-{
-  if (!reset_btn)
-    return ;
-  Serial.println("___ RESET ___");
-  alarm_on = false;
-}
+bool  alarm_on = false;
 
 void pinModes()
 {
   pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(SENSOR_IR_PIN, INPUT);
 }
 
 void initProperties()
 {
   ArduinoCloud.setBoardId(DEVICE_LOGIN_NAME);
   ArduinoCloud.setSecretDeviceKey(DEVICE_SECRET);
-  // ArduinoCloud.addProperty(sensor, READ, 5 * SECONDS, NULL);
-  ArduinoCloud.addProperty(sensor, READ, ON_CHANGE, NULL);
-  ArduinoCloud.addProperty(reset_btn, READWRITE, ON_CHANGE, onResetBtnChange);
+  ArduinoCloud.addProperty(sensor_gas, READ, 2 * SECONDS, NULL);
+  ArduinoCloud.addProperty(sensor_gas_threshold, READWRITE, ON_CHANGE, ir_thresh_cb);
+  ArduinoCloud.addProperty(reset_btn, READWRITE, ON_CHANGE, reset_btn_cb);
+  ArduinoCloud.addProperty(sensor_ir, READ, ON_CHANGE, NULL);
 }
 
 void setup() {
@@ -38,30 +28,43 @@ void setup() {
   Serial.println("___ SETUP ___");
   pinModes();
   initProperties();
-  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
   Serial.println("___ WAITING CONNECTION ___");
-  while (ArduinoCloud.connected())
-    delay(1000);
+  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
+  while (ArduinoIoTPreferredConnection.check() != NetworkConnectionState::CONNECTED)
+    delay(3000);
   Serial.println("___ CONNECTED ___");
+  tone(BUZZER_PIN, NOTE_A4, 200);
+  delay(100);
+  sensor_ir = true;
   #if LOG_LEVEL > 0
     setDebugMessageLevel(LOG_LEVEL);
     ArduinoCloud.printDebugInfo();
   #endif
 }
 
-// Operates in a deffrent loop until it returns false.
-bool alarm_cb()
-{
-  if (!alarm_on)
-    return false;
-  ArduinoCloud.update();
-  return true;
-}
-
 void loop() {
   ArduinoCloud.update();
-  sensor++;
-  if (sensor == 10)
-    sensor = 0;
+  while (ArduinoIoTPreferredConnection.check() != NetworkConnectionState::CONNECTED)
+  {
+    Serial.println("__ DISCONNECTED __");
+    // Beep. Beep.. ... Beep. Beep.. :(
+    tone(BUZZER_PIN, NOTE_B3, 100);
+    delay(300);
+    tone(BUZZER_PIN, NOTE_B3, 200);
+    delay(1400);
+    return ;
+  }
+  sensor_gas = analogRead(SENSOR_MQ_PIN);
+  sensor_ir = !digitalRead(SENSOR_IR_PIN);
+  Serial.print("Gas_Sensor:");
+  Serial.print(sensor_gas);
+  Serial.print(",");
+  Serial.print("Gas_Threshold:");
+  Serial.print(sensor_gas_threshold);
+  Serial.print(",");
+  Serial.print("IR_Sensor:");
+  Serial.println(sensor_ir);
+  if (sensor_gas > sensor_gas_threshold && !sensor_ir)
+    alarm_on = true;
   alarm(alarm_cb);
 }
